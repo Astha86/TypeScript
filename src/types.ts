b3411fb7a40370a8736e90a2d387a3e9ccc538f8
Astha86 <asthasahani08@gmail.com>
2026-01-20 22:16:59 +0530

    Types in TypeScript

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+// Type Inferencing - It will assume the type
+
+let code = "TypeScript"; // string
+let num = 86; // number
+let random = Math.random() > 0.5 ? 10 : '5'; // string | number
+
+
+// Type Annotations - We have to explicitly specify types using annotations
+
+let myNumber: number = 86;
+let myString: string = "Hello, TypeScript!";
